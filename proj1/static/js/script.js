d0196ee4a10b53f980683c78ab968d56526d0cfa
unknown <41454606+enigmaedu@users.noreply.github.com>
2021-01-03 15:14:02 -0800

    updated code-index, css, js

---
+++ proj1/static/js/script.js
@@ -0,0 +1,11 @@
+// challenge 1 : age in days
+function ageInDays(){
+    var birthYear=prompt('What year were you born..Good Friend?');
+    var ageInDays=(2021 - birthYear) * 365;
+    var h1=document.createElement('h1');
+    var textAnswer=document.createTextNode('You are ' + ageInDays + ' days old');
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById('flex-box-result').appendChild(h1);
+    // console.log(ageInDays);
+}
