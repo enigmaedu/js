f4bdbcd974a12ce0bba69fcb9f779342a1c799dc
unknown <41454606+enigmaedu@users.noreply.github.com>
2021-01-03 18:47:55 -0800

    updated code-index, css, js

--- proj1/static/js/script.js
+++ proj1/static/js/script.js
@@ -9,3 +9,7 @@ function ageInDays(){
     document.getElementById('flex-box-result').appendChild(h1);
     // console.log(ageInDays);
 }
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
