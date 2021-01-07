f2cf0f7f14db169245cfe40d8cdd2a64160f32a9
unknown <41454606+enigmaedu@users.noreply.github.com>
2021-01-06 22:13:08 -0800

    project coding is complete

--- proj4/static/js/script.js
+++ proj4/static/js/script.js
@@ -102,3 +102,64 @@ function rpsFrontEnd(humanImageChoice, botImageChoice, finalMessage) {
 
 }
 
+// Challenge 4: Change the color of all Buttons
+var all_buttons=document.getElementsByTagName('button');
+console.log(all_buttons);
+
+// Take a backup of the original button colors
+var copyAllButtons=[];
+for (let i=0; i < all_buttons.length; i++) {
+    copyAllButtons.push(all_buttons[i]);
+}
+console.log('Copy of initial button state: ',copyAllButtons);
+
+// main function that acts a switch
+function buttonColorChange(buttonThingy) {
+     if(buttonThingy.value==='red'){
+         buttonsRed();
+     } else if(buttonThingy.value==='green') {
+         buttonsGreen();
+     } else if(buttonThingy.value==='reset') {
+        //  console.log(copyAllButtons);
+         buttonsColorReset();
+     } else if(buttonThingy.value==='random'){
+         randomColors();
+     }
+}
+
+
+function buttonsRed() {
+    for (let i=0; i<all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
+    }
+}
+
+function buttonsGreen() {
+    for (let i=0; i<all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonsColorReset() {
+    for (let i=0; i<all_buttons.length; i++) {
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        // all_buttons[i].classList.add(copyAllButtons[i]); //NOT Working
+        // all_buttons[i].classList.add(copyAllButtons[i].classList[1]); //alternate - NOT Working
+        // console.log(copyAllButtons[i].classList[1]);
+        // all_buttons[i].classList.add('btn-warning'); // debug-works
+        //  console.log(copyAllButtons[i]); debug
+        
+        // copyAllButtons[i]
+    }
+}
+
+function randomColors() {
+    let choices=['btn-primary','btn-danger','btn-success','btn-warning'];
+    for(let i=0; i<all_buttons.length; i++) {
+        let randomNumber=Math.floor(Math.random()*4);
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+    }
+}
